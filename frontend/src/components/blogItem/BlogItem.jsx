cce47b1f881c75d9ed3d388a9ede8532ab29b7de
Dipan Sarkar <alexmithun1005@gmial.com>
2024-11-09 14:52:15 +0530

    frontend fixes

--- frontend/src/components/blogItem/BlogItem.jsx
+++ frontend/src/components/blogItem/BlogItem.jsx
@@ -1,27 +1,37 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { MdOutlineVerified } from "react-icons/md";
+import "./blogitem.css";
 
 const BlogItem = ({ blogs, handleDelete }) => {
   return (
     <div>
       {blogs.map((blog) => {
+        const shortContent = blog.content.split(" ").slice(0, 20).join(" ");
+
         return (
           <div
             key={blog._id}
-            className="border-b-2  p-2 flex justify-evenly gap-4 p-3 "
+            className=" blogItem border-b-2  p-2 flex justify-between gap-4 p-3 "
           >
             <div>
-              <p className="text-slate-900 text-[14px] capitalize font-normal">
+              <p className="text-slate-900 text-[14px] capitalize font-normal inline-block">
                 {" "}
                 By : {blog.author}
+                <span className="inline-block align-middle ms-1">
+                  <MdOutlineVerified color="skyblue" />
+                </span>
               </p>
-              <h1 className="text-[24px] font-bold capitalize leading-[30px] mt-2">
+              <h1 className="text-[24px] font-bold capitalize leading-[30px] mt-2 tracking-wide">
                 {blog.title}
               </h1>
               <p className="text-[#6B6B6B] font-normal text-[16px] my-4 leading-[20px]">
-                {blog.content}{" "}
-                <Link className="text-slate-900" to={`/blogs/${blog._id}`}>
+                {shortContent}{" "}
+                <Link
+                  className="text-slate-900 font-semibold"
+                  to={`/blogs/${blog._id}`}
+                >
                   See more...
                 </Link>
               </p>
@@ -34,7 +44,7 @@ const BlogItem = ({ blogs, handleDelete }) => {
               </button>
             </div>
             <img
-              className="w-[200px] h-[200px] object-cover"
+              className="w-[200px] h-[220px] object-cover"
               src={blog.image}
               alt=""
             />
